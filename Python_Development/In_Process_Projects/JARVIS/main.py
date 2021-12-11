b2f33fe40b2f5e25019eef62c8acac72e9206319
TroyPetrowicz <tpetrowicz@empiredynamics.us>
2021-12-11 13:52:34 -0500

    Integration Updates

--- Python_Development/In_Process_Projects/JARVIS/main.py
+++ Python_Development/In_Process_Projects/JARVIS/main.py
@@ -1,7 +1,78 @@
 import requests
-from functions.online_ops import find_my_ip, get_latest_news, get_random_advice, get_random_joke, get_trending_movies, get_weather_report, play_on_youtube, search_on_google, search_on_wikipedia, send_email, send_whatsapp_message
+from functions.online_ops import find_my_ip, get_random_advice, get_random_joke, play_on_youtube, search_on_google, search_on_wikipedia, send_email, send_whatsapp_message
 from functions.os_ops import open_calculator, open_camera, open_cmd, open_notepad, open_discord
 from pprint import pprint
+from datetime import datetime
+import pyttsx3
+from decouple import config
+import speech_recognition as sr
+from random import choice
+from utils import opening_text
+
+
+USERNAME = config('USER')
+BOTNAME = config('BOTNAME')
+
+engine = pyttsx3.init('sapi5')
+
+# Set Rate
+engine.setProperty('rate', 190)
+
+# Set Volume
+engine.setProperty('volume', 1.0)
+
+# Set Voice (Female)
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+
+
+# Text to Speech Conversion
+def speak(text):
+    """Used to speak whatever text is passed to it"""
+    
+    engine.say(text)
+    engine.runAndWait()
+
+
+def greet_user():
+    """Greets the user according to the time"""
+    
+    hour = datetime.now().hour
+    if (hour >= 6) and (hour < 12):
+        speak(f"Good Morning {USERNAME}")
+    elif (hour >= 12) and (hour < 16):
+        speak(f"Good afternoon {USERNAME}")
+    elif (hour >= 16) and (hour < 19):
+        speak(f"Good Evening {USERNAME}")
+    speak(f"I am {BOTNAME}. How may I assist you?")
+
+
+def take_user_input():
+    """Takes user input, recognizes it using Speech Recognition module and converts it into text"""
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print('Listening....')
+        r.pause_threshold = 2
+        audio = r.listen(source)
+
+    try:
+        print('Recognizing...')
+        query = r.recognize_google(audio, language='en-in')
+        if not 'exit' in query or 'stop' in query:
+            speak(choice(opening_text))
+        else:
+            hour = datetime.now().hour
+            if hour >= 21 and hour < 6:
+                speak("Good night sir, take care!")
+            else:
+                speak('Have a good day sir!')
+            exit()
+    except Exception:
+        speak('Sorry, I could not understand. Could you please say that again?')
+        query = 'None'
+    return query
 
 
 if __name__ == '__main__':
@@ -81,23 +152,23 @@ if __name__ == '__main__':
             speak("For your convenience, I am printing it on the screen sir.")
             pprint(advice)
 
-        elif "trending movies" in query:
-            speak(f"Some of the trending movies are: {get_trending_movies()}")
-            speak("For your convenience, I am printing it on the screen sir.")
-            print(*get_trending_movies(), sep='\n')
+        # elif "trending movies" in query:
+        #     speak(f"Some of the trending movies are: {get_trending_movies()}")
+        #     speak("For your convenience, I am printing it on the screen sir.")
+        #     print(*get_trending_movies(), sep='\n')
 
-        elif 'news' in query:
-            speak(f"I'm reading out the latest news headlines, sir")
-            speak(get_latest_news())
-            speak("For your convenience, I am printing it on the screen sir.")
-            print(*get_latest_news(), sep='\n')
+        # elif 'news' in query:
+        #     speak(f"I'm reading out the latest news headlines, sir")
+        #     speak(get_latest_news())
+        #     speak("For your convenience, I am printing it on the screen sir.")
+        #     print(*get_latest_news(), sep='\n')
 
-        elif 'weather' in query:
-            ip_address = find_my_ip()
-            city = requests.get(f"https://ipapi.co/{ip_address}/city/").text
-            speak(f"Getting weather report for your city {city}")
-            weather, temperature, feels_like = get_weather_report(city)
-            speak(f"The current temperature is {temperature}, but it feels like {feels_like}")
-            speak(f"Also, the weather report talks about {weather}")
-            speak("For your convenience, I am printing it on the screen sir.")
-            print(f"Description: {weather}\nTemperature: {temperature}\nFeels like: {feels_like}")
+        # elif 'weather' in query:
+        #     ip_address = find_my_ip()
+        #     city = requests.get(f"https://ipapi.co/{ip_address}/city/").text
+        #     speak(f"Getting weather report for your city {city}")
+        #     weather, temperature, feels_like = get_weather_report(city)
+        #     speak(f"The current temperature is {temperature}, but it feels like {feels_like}")
+        #     speak(f"Also, the weather report talks about {weather}")
+        #     speak("For your convenience, I am printing it on the screen sir.")
+        #     print(f"Description: {weather}\nTemperature: {temperature}\nFeels like: {feels_like}")
