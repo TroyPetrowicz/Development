5ba2c886ca1025d331d0729ef8fb1c7a754b2f3f
TroyPetrowicz <tpetrowicz@empiredynamics.us>
2022-04-26 17:31:51 -0400

    Add menu screens

--- Application_Development/Movie_Database/Movie_db_UI_elements.py
+++ Application_Development/Movie_Database/Movie_db_UI_elements.py
@@ -8,3 +8,15 @@ class home_screen():
 
     def __init__(self):
         print("I hate it here")
+        
+
+class search_screen():
+    
+    def __init__(self):
+        print("I hate it here more")
+        
+
+class view_all():
+    
+    def __init__(self) -> None:
+        print("I really hate it here")
